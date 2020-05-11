85f3e08e728991c5332b2457a9c1ac34427a051e
Israel Miguel Saura <israelmiguelsaura@gmail.com>
2020-05-11 18:25:11 +0200

    commit con las rutas de nuestra aplicacion

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,52 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/proyectos', function () {
+    return "Aqui ira la pestaña proyectos";
+});
+
+Route::get('/proyectos', function () {
+    return "Aqui ira la pestaña proyectos";
+});
+
+Route::get('/Compañeros', function () {
+    return "Aqui ira la pestaña con los compañeros del proyecto";
+});
+
+Route::get('/usuarios', function () {
+    return "Aqui ira la pestaña usuarios";
+});
+
+Route::get('/usuarios/gestion_de_usuarios', function () {
+    return "Aqui ira la pagina con formularios (añadir, actualizar, leer, etc)";
+});
+
+Route::get('/estadisticas', function () {
+    return "Aqui ira la pestaña estadisticas en tiempo";
+});
+
+Route::get('/mis_notas', function () {
+    return "Aqui ira la pagina con las notas";
+});
+Route::get('/mis_notas/gestion_de_notas', function () {
+    return "Aqui ira la pagina con formularios (añadir, actualizar, leer, etc)";
+});
+
+Route::get('/perfil', function () {
+    return "Aqui ira la pagina con las notas";
+});
+Route::get('/perfil/gestion_de_perfil', function () {
+    return "Aqui ira la pagina con formularios (añadir, actualizar, leer, etc)";
+});
+
+Route::get('/historial', function () {
+    return "Aqui ira la pagina con el historial";
+});
+
+Route::get('/busqueda', function () {
+    return "Aqui ira la pagina de busqueda por campo nombre de proyecto";
+});
+Route::get('/busqueda/avanzada', function () {
+    return "Aqui ira la pagina con el formulario de busqueda por campos";
+});
+
