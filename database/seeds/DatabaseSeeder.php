d0718d215ffe477dd3de4381226fef290be84901
Israel Miguel Saura <israelmiguelsaura@gmail.com>
2020-05-11 17:00:05 +0200

    commit con el seeder de la tabla rol.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->truncateTablas([
+            'rol'
+        ]);
+        $this->call(tablaRolSeeder::class);
+    }
+    protected function truncateTablas(array $tablas)
+    {
+        //Esta funcion sirve para truncar (vaciar) las tablas en caso de tener que volver a rellenarlas con datos fijos
+        //Solo se usa en el entorno de desarrollo.
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;'); //deshabilito la clave foranea para evitar problemas
+        foreach ($tablas as $tabla){
+            DB::table($tabla)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;'); //habilito de nuevo
     }
 }
---
+++ database/seeds/tablaRolSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon; //Esta libreria sirve para añadir timestamps del momento (fechas) en el formato indicado.
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class tablaRolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            'MASTER' => 'El superusuario de la aplicacion, tiene acceso total a la bbdd',
+            'FINAL_BOSS' => 'Por debajo de Master pero con la capacidad de crear proyectos y gestionarlos en su totalidad',
+            'PROJECT_MASTER' => 'El MAESTRO DE PROYECTOS, tiene la capacidad de gestionar de los proyectos ya creados, sus usuario y las tareas del proyecto.',
+            'TASK_MASTER' => 'El MAESTRO DE TAREAS, tiene la capacidad de añadir, modificar y finalizar tareas (NO BORRAR) de los proyectos ya creados.',
+            'USER' => 'El usuario, solo puede gestionar las tareas creadas por el o en la que este participando en ese momento (Iniciar, pausar y finalizar)'
+        ];
+        foreach($roles as $key => $value){
+
+                DB::table('rol')->insert([
+                    'nombre' => $key,
+                    'descripcion_rol' => $value,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]);
+
+        };
+    }
+}
